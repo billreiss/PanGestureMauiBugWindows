cf35ed2c4cd3911577819986dd0d758ce0f1363c
Bill Reiss <bireis@microsoft.com>
2024-02-06 12:46:17 -0500

    Add project files.

---
+++ PanGestureSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PanGestureSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PanGestureSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PanGestureSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
